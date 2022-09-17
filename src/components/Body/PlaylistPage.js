509fd5ee96ef3f9e5023f0169770a07fc0db9e59
abidinco <sav.abidin@gmail.com>
2022-09-17 23:34:32 +0300

    feat(playlist): add playlist-item

--- src/components/Body/PlaylistPage.js
+++ src/components/Body/PlaylistPage.js
@@ -11,7 +11,7 @@ const PlaylistPage = () => {
                 </div>
                 <div className={styles['header-info']}>
                     <div className={styles['header-info-type']}>Playlist</div>
-                    <div className={styles['header-info-name']}>Liked Songs | Playlist Name</div>
+                    <div className={styles['header-info-name']}>Liked Songs</div>
                     <div className={styles['header-info-bottom']}><img src="/pp.jfif" width={24} height={24} alt="Pp" /> <span>Abidin A.</span> • 2 songs, <span>15 min 28 sec</span></div>
                 </div>
             </div>
@@ -21,74 +21,34 @@ const PlaylistPage = () => {
                 </div>
             </div>
             <div className={styles.table}>
-                <div className={styles['table-header']}># Title Album Date Added Duration</div>
+                <div className={styles['table-header']}>
+                    <span style={{ fontSize: 16, fontWeight: 300 }}>#</span>
+                    <span>Title</span>
+                    <span>Album</span>
+                    <span>Date Added</span>
+                    <span><Icon name="duration" height={16} width={16} color="#b3b3b3" /></span>
+                </div>
                 <div className={styles.playlist}>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
-                    <div>Song</div>
+                    <div className={styles.item}>
+                        <div className={styles.index}>
+                            <div className={styles['index-number']}>1</div>
+                            <div className={styles['index-icon']}>
+                                <Icon name="player-play" width={16} height={16} color="#fff" />
+                            </div>
+                        </div>
+                        <div className={styles.meta}>
+                            <img src="/browse-card-images/decades.jfif" alt="Song" />
+                            <div>
+                                <div className={styles['song-name']}>Gnossienne No.1</div>
+                                <div>Erkan Oğur</div>
+                            </div>
+                        </div>
+                        <div>Dönmez Yol</div>
+                        <div className={styles.date}>Dec 5, 2021</div>
+                        <div className={styles.duration}>
+                            <span>4:39</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
