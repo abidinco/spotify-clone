d24610b6cf8bf2ac4f9db8879b4de2ae431c72aa
abidinco <sav.abidin@gmail.com>
2023-01-01 23:26:45 +0300

    feat(api): trying to automatically login my user profile

--- src/spotify/api.js
+++ src/spotify/api.js
@@ -27,7 +27,7 @@ const scopes = [
 
 export const LOGIN_URL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scopes.join(
   "%20"
-)}&response_type=token&show_dialog=true`;
+)}&response_type=token`;
 
 const Spotify = {
   getNow() {
